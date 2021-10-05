a9cf0ea3a51cd076a3d66f21dfd2c12bcdc594c6
Celestin <scelestinherbegeorge@gmail.com>
2021-10-05 16:17:24 +0200

    updated config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ASTRON Apertif DR1'
+copyright = '2021, CHG'
+author = 'C Herbe-George'
 
 release = '0.1'
 version = '0.1.0'
